017b1d7f30a3b14a6d6df45ce2f31945fe98c913
Matt Co <mco@athenahealth.com>
2021-12-09 11:18:23 -0600

    iadded exercise and weightinput

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export function App(){
+  return (<>
+  <h1> Gymrat</h1>
+  <form>
+    <div> 
+    <label htmlFor = "exercise">
+    Exercise
+    </label> 
+    <br/> 
+    <input type="text" id="exercise"/>
     </div>
-  );
+    <div> 
+    <label htmlFor = "weight">
+    Weight
+    </label>
+    <br/> 
+    <input type="text" id="weight"/>
+   
+    </div>
+   
+  </form>
+  </>)
 }
-
-export default App;
