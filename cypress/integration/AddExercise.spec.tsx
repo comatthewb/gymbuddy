eb35a18457b3e2bc00f4c4e1b76a483012524f47
Matt Co <mco@athenahealth.com>
2021-12-14 10:22:48 -0600

    added describe block

--- cypress/integration/AddExercise.spec.tsx
+++ cypress/integration/AddExercise.spec.tsx
@@ -1,30 +1,32 @@
-beforeEach(() => {
-    cy.visit("http://localhost:3000/add");
-});
+describe("AddExercise", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/add");
+    });
 
-it("should support adding an exercise", () => {
-    //should require all fields, so submit the form empty
-    cy.findByRole("button", { name: "Save Exercise" }).click();
+    it("should support adding an exercise", () => {
+        //should require all fields, so submit the form empty
+        cy.findByRole("button", { name: "Save Exercise" }).click();
 
-    //Now, validation error message should display
-    cy.findByText("Please enter a name for the exercise.");
+        //Now, validation error message should display
+        cy.findByText("Please enter a name for the exercise.");
 
-    cy.findAllByLabelText("Exercise").type("Hula hoop");
-    cy.findAllByLabelText("Weight").type("5");
-    cy.findByRole("button", { name: "Save Exercise" }).click();
+        cy.findAllByLabelText("Exercise").type("Hula hoop");
+        cy.findAllByLabelText("Weight").type("5");
+        cy.findByRole("button", { name: "Save Exercise" }).click();
 
-    // Confirm saved data displays on home page.
-    cy.findByText("Hula hoop");
-    cy.findByText("5");
-});
+        // Confirm saved data displays on home page.
+        cy.findByText("Hula hoop");
+        cy.findByText("5");
+    });
 
-it("should validate onBlur", () => {
-    //Initially, no validation errors should display
-    cy.findByText("Please enter a name for the exercise.").should("not.exist");
-    cy.findAllByLabelText("Exercise").focus().blur();
-    cy.findAllByLabelText("Weight").focus().blur();
+    it("should validate onBlur", () => {
+        //Initially, no validation errors should display
+        cy.findByText("Please enter a name for the exercise.").should("not.exist");
+        cy.findAllByLabelText("Exercise").focus().blur();
+        cy.findAllByLabelText("Weight").focus().blur();
 
-    //Now, validation error message should display
-    cy.findByText("Please enter a name for the exercise.");
-    cy.findByText("Please enter a weight for the exercise.");
+        //Now, validation error message should display
+        cy.findByText("Please enter a name for the exercise.");
+        cy.findByText("Please enter a weight for the exercise.");
+    });
 });
--- cypress/integration/Home.spec.tsx
+++ cypress/integration/Home.spec.tsx
@@ -1,6 +1,9 @@
-beforeEach(() => {
-    cy.visit("http://localhost:3000");
-});
-it("should display 'no exercises' message when there are no exercises", () => {
-    cy.findByText("No exercises exist.");
+describe("Home", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+    });
+
+    it("should display 'no exercises' message when there are no exercises", () => {
+        cy.findByText("No exercises exist.");
+    });
 });
